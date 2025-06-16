74a00f46c517a860686b35722eed90639bd136cf
EduardoMedeirosGalvao <galvaoe378@gmail.com>
2025-06-16 22:21:13 +0000

    Desafio Xandrez

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,40 @@
-#include <stdio.h
+#include <stdio.h>
 
 int main() {
-    Printf("Desafio Xadrez!\n");
-    printf("Novo Commit\n");
+    int n, i;
+    int pontosA = 0, pontosB = 0;
+
+    printf("Quantas peças cada jogador terá? ");
+    scanf("%d", &n);
+
+    int forcaA[n], forcaB[n];
+
+    printf("Digite as forças das peças do Jogador A:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &forcaA[i]);
+    }
+
+    printf("Digite as forças das peças do Jogador B:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &forcaB[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(forcaA[i] > forcaB[i]) {
+            pontosA++;
+        } else if(forcaB[i] > forcaA[i]) {
+            pontosB++;
+        }
+    }
+
+    printf("\nResultado:\n");
+    if(pontosA > pontosB) {
+        printf("Jogador A venceu com %d pontos!\n", pontosA);
+    } else if(pontosB > pontosA) {
+        printf("Jogador B venceu com %d pontos!\n", pontosB);
+    } else {
+        printf("Empate! Ambos fizeram %d pontos.\n", pontosA);
+    }
+
+    return 0;
 }
